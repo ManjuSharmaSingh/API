99211e79165027bbaf148d01d70a5b26e7039db9
ManjuSharmaSingh <33419287+ManjuSharmaSingh@users.noreply.github.com>
2019-06-27 10:35:53 +0530

    Update docker.sh
    change the docker tag

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
-docker pull manju13/omdbapi_code:api_code
-docker run --name OMDBAPI_CONTAINER -td docker.io/manju13/omdbapi_code:api_code
+docker pull manju13/omdbapi_code:RottenTomato
+docker run --name OMDBAPI_CONTAINER -td docker.io/manju13/omdbapi_code:RottenTomato
 docker exec -it OMDBAPI_CONTAINER bash
